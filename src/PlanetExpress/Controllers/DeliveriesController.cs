708a3eeb44739b63e3de7a92027a00ef5d6b0f88
kstaruch <kstaruch@gmail.com>
2015-01-22 10:57:33 +0100

    Level_0

---
+++ src/PlanetExpress/Controllers/DeliveriesController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PlanetExpress.Controllers
+{
+    public class DeliveriesController : ApiController
+    {
+        public DeliveriesController()
+        {
+            Deliveries = new List<Delivery>(new[]
+            {
+                new Delivery
+                {
+                    Id = "dev1",
+                    Summary = "some dev1 summary",
+                    Origin = "origin1",
+                    Desination = "destination1",
+                    PlacedAt = DateTime.Now,
+                    PlacedBy = "some_guy1",
+                    Status = "Good news, everyone!"
+                }, 
+                new Delivery
+                {
+                    Id = "dev2",
+                    Summary = "some dev2 summary",
+                    Origin = "origin2",
+                    Desination = "destination2",
+                    PlacedAt = DateTime.Now,
+                    PlacedBy = "some_guy2",
+                    Status = "Good news, everyone!"
+                }, 
+                new Delivery
+                {
+                    Id = "dev3",
+                    Summary = "some dev3 summary",
+                    Origin = "origin3",
+                    Desination = "destination3",
+                    PlacedAt = DateTime.Now,
+                    PlacedBy = "some_guy3",
+                    Status = "Good news, everyone!"
+                }
+            });
+
+        }
+
+        public IEnumerable<Delivery> Get()
+        {   
+            return Deliveries;
+        }
+
+        [HttpPut]
+        public void ChangeStatus(string id, string status)
+        {
+            var delivery = Deliveries.FirstOrDefault(d => d.Id == id);
+            if (delivery != default(Delivery))
+            {
+                delivery.ChangeStatus(status);
+            }
+
+        }
+
+        public IList<Delivery> Deliveries { get; protected set; }
+    }
+
+    public class Delivery
+    {
+        public string Id { get; set; }
+        public string Summary { get; set; }
+        public string Origin { get; set; }
+        public string Desination { get; set; }
+        public string Status { get; set; }
+        public DateTime PlacedAt { get; set; }
+        public string PlacedBy { get; set; }
+
+        public void ChangeStatus(string status)
+        {
+            Status = status;
+        }
+    }
+
+    
+
+    
+
+}
---
+++ src/PlanetExpress/Controllers/PersonnelController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PlanetExpress.Controllers
+{
+    public class PersonnelController : ApiController
+    {
+        private readonly IEnumerable<Employee> employees;
+
+        public PersonnelController()
+        {
+            this.employees = new Employee[]
+            {
+                new Employee("Philip J. Fry"), 
+                new Employee("Turanga Leela"),
+                new Employee("Bender Bending Rodriguez"),
+            };
+
+        }
+
+        public IEnumerable<Employee> Get()
+        {
+            return employees;
+        }
+
+    }
+
+    public class Employee
+    {
+        public Employee(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
